6f96ad6ad544da9c8356ff238e55f5384546a014
Tomeee <diether.noche@gmail.com>
2023-03-01 19:48:39 +0800

    Added VapeHub Logo

--- src/login/index.jsx
+++ src/login/index.jsx
@@ -37,42 +37,46 @@ function Login() {
       });
   };
   return !isUserLoggedIn ? (
-    <form className="justify-content-center align-items-center">
-      <div className="form-group ">
-        <input
-          type="email"
-          className="form-control"
-          id="username"
-          aria-describedby="emailHelp"
-          placeholder="Enter email"
-        />
+    <div className="row row-cols-2">
+      <div className="col">
+        <img src="./assets/images/Vape-hub.svg" alt="Vapehub-Logo" />
       </div>
-      <div className="form-group my-3">
-        <input
-          type="password"
-          className="form-control"
-          id="password"
-          placeholder="Password"
-        />
-      </div>
-      <div className="form-check mb-3">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="exampleCheck1"
-        />
-        <label className="form-check-label" htmlFor="exampleCheck1">
-          Remember me
-        </label>
-      </div>
-      <button
-        type="submit"
-        className="btn btn-lg w-75 btn-primary"
-        onClick={login}
-      >
-        Login
-      </button>
-    </form>
+      <form className="justify-content-center align-items-center">
+        <div className="form-group ">
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            placeholder="Enter Username"
+          />
+        </div>
+        <div className="form-group my-3">
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Password"
+          />
+        </div>
+        <div className="form-check mb-3">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Remember me
+          </label>
+        </div>
+        <button
+          type="submit"
+          className="btn btn-lg w-75 btn-primary"
+          onClick={login}
+        >
+          Login
+        </button>
+      </form>
+    </div>
   ) : (
     <></>
   );
