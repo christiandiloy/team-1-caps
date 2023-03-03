36d4192e89c1ff1bdf2e7a0517e1ccdce8f9ad77
Tomeee <diether.noche@gmail.com>
2023-03-04 00:18:42 +0800

    Finalize style for sign in page

--- src/login/index.jsx
+++ src/login/index.jsx
@@ -16,25 +16,22 @@ function Login() {
     <>
       <div className="login-home">
         <LoginNav />
-        <div className="login-bg row row-cols-1 row-cols-lg-2 justify-content-center align-items-center">
-          <div className="col">
-            <img
-              className="login-logo img-fluid"
-              src="./assets/images/Vape-hub.svg"
-              alt="Vapehub-Logo"
-            />
-          </div>
-          <div className="col border rounded bg-light p-5">
-            <h2 className="mb-5">{isSignIn ? "Log In" : "Sign Up"}</h2>
+        <div className="container login-container">
+          <div className="border rounded bg-light p-5">
             {isSignIn ? <SignIn /> : <SignUp />}
-            <p>
-              {isSignIn ? "Don't have an account?" : "Already have an account?"}
+            <p className="login-plink ms-3">
+              {isSignIn
+                ? "Don't have an account yet?"
+                : "Already have an account?"}{" "}
+              &nbsp;
+              <a className="login-link link-warning" onClick={toggleSignInUp}>
+                {isSignIn ? "Sign up here" : "Sign in"}
+              </a>
+              {isSignIn ? "" : " here instead"}
             </p>
-            <button onClick={toggleSignInUp}>
-              {isSignIn ? "Sign up here" : "Sign in here instead"}
-            </button>
           </div>
         </div>
+
         <Footer />
       </div>
     </>
--- src/login/signIn.jsx
+++ src/login/signIn.jsx
@@ -37,40 +37,53 @@ function SignIn() {
   };
   return !isUserLoggedIn ? (
     <>
-      <div className="">
-        <div className="form-group w-50">
-          <input
-            type="text"
-            className="form-control"
-            id="username"
-            placeholder="Enter Username"
-          />
+      <div className="row justify-content-center align-items-center g-0 ">
+        <div className="container col me-3">
+          <h2 className="lead display-6 text-muted mb-5">
+            Login to your account.
+          </h2>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="username"
+              placeholder="Enter Username"
+            />
+          </div>
+          <div className="form-group my-3">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Password"
+            />
+          </div>
+          <div className="form-check mb-3">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Remember me
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="btn btn-lg w-100 btn-warning text-light"
+            onClick={login}
+          >
+            Login
+          </button>
         </div>
-        <div className="form-group w-50 my-3">
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            placeholder="Password"
+        <div className="col text-center border-start border-warning">
+          <img
+            className="login-logo img-fluid"
+            src="./assets/images/Vape-hub.svg"
+            alt="Vapehub-Logo"
           />
+          <h1 className="login-brand">VAPE HUB</h1>
         </div>
-        <div className="form-check mb-3">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id="exampleCheck1"
-          />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            Remember me
-          </label>
-        </div>
-        <button
-          type="submit"
-          className="btn btn-lg w-50 btn-warning text-light"
-          onClick={login}
-        >
-          Login
-        </button>
       </div>
     </>
   ) : (
