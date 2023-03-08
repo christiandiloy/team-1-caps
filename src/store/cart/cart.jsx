cd8f92b9dc6b3428aa09a2fcc2c801888abcd135
Tomeee <diether.noche@gmail.com>
2023-03-08 23:22:52 +0800

    Add paypal button and cash on delivery button

--- src/store/cart/cart.jsx
+++ src/store/cart/cart.jsx
@@ -2,59 +2,65 @@ import React from "react";
 import "./cart.css";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
+const initialOptions = {
+  "client-id": `${process.env.REACT_APP_PAYPAL_KEY}`,
+};
+const product = { description: "Aegis Legend", price: 2500.0 };
+
 function MyCart() {
   return (
     <>
-      <section class="shopping-cart dark">
-        <div class="container">
-          <div class="block-heading">
+      <section className="shopping-cart dark">
+        <div className="container">
+          <div className="block-heading">
             <h2>Shopping Cart</h2>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam
               urna, dignissim nec auctor in, mattis vitae leo.
             </p>
           </div>
-          <div class="content">
-            <div class="row">
-              <div class="col-md-12 col-lg-8">
-                <div class="items">
-                  <div class="product">
-                    <div class="row">
-                      <div class="col-md-3">
+          <div className="content">
+            <div className="row">
+              <div className="col-md-12 col-lg-8">
+                <div className="items">
+                  <div className="product">
+                    <div className="row">
+                      <div className="col-md-3">
                         <img
-                          class="img-fluid mx-auto d-block image"
+                          className="img-fluid mx-auto d-block image"
                           src="image.jpg"
                         />
                       </div>
-                      <div class="col-md-8">
-                        <div class="info">
-                          <div class="row">
-                            <div class="col-md-5 product-name">
-                              <div class="product-name">
+                      <div className="col-md-8">
+                        <div className="info">
+                          <div className="row">
+                            <div className="col-md-5 product-name">
+                              <div className="product-name">
                                 <a href="#">Lorem Ipsum dolor</a>
-                                <div class="product-info">
+                                <div className="product-info">
                                   <div>
-                                    Display: <span class="value">5 inch</span>
+                                    Display:{" "}
+                                    <span className="value">5 inch</span>
                                   </div>
                                   <div>
-                                    RAM: <span class="value">4GB</span>
+                                    RAM: <span className="value">4GB</span>
                                   </div>
                                   <div>
-                                    Memory: <span class="value">32GB</span>
+                                    Memory: <span className="value">32GB</span>
                                   </div>
                                 </div>
                               </div>
                             </div>
-                            <div class="col-md-4 quantity">
-                              <label for="quantity">Quantity:</label>
+                            <div className="col-md-4 quantity">
+                              <label htmlFor="quantity">Quantity:</label>
                               <input
                                 id="quantity"
                                 type="number"
                                 value="1"
-                                class="form-control quantity-input"
+                                className="form-control quantity-input"
                               />
                             </div>
-                            <div class="col-md-3 price">
+                            <div className="col-md-3 price">
                               <span>$120</span>
                             </div>
                           </div>
@@ -62,43 +68,44 @@ function MyCart() {
                       </div>
                     </div>
                   </div>
-                  <div class="product">
-                    <div class="row">
-                      <div class="col-md-3">
+                  <div className="product">
+                    <div className="row">
+                      <div className="col-md-3">
                         <img
-                          class="img-fluid mx-auto d-block image"
+                          className="img-fluid mx-auto d-block image"
                           src="image.jpg"
                         />
                       </div>
-                      <div class="col-md-8">
-                        <div class="info">
-                          <div class="row">
-                            <div class="col-md-5 product-name">
-                              <div class="product-name">
+                      <div className="col-md-8">
+                        <div className="info">
+                          <div className="row">
+                            <div className="col-md-5 product-name">
+                              <div className="product-name">
                                 <a href="#">Lorem Ipsum dolor</a>
-                                <div class="product-info">
+                                <div className="product-info">
                                   <div>
-                                    Display: <span class="value">5 inch</span>
+                                    Display:{" "}
+                                    <span className="value">5 inch</span>
                                   </div>
                                   <div>
-                                    RAM: <span class="value">4GB</span>
+                                    RAM: <span className="value">4GB</span>
                                   </div>
                                   <div>
-                                    Memory: <span class="value">32GB</span>
+                                    Memory: <span className="value">32GB</span>
                                   </div>
                                 </div>
                               </div>
                             </div>
-                            <div class="col-md-4 quantity">
-                              <label for="quantity">Quantity:</label>
+                            <div className="col-md-4 quantity">
+                              <label htmlFor="quantity">Quantity:</label>
                               <input
                                 id="quantity"
                                 type="number"
                                 value="1"
-                                class="form-control quantity-input"
+                                className="form-control quantity-input"
                               />
                             </div>
-                            <div class="col-md-3 price">
+                            <div className="col-md-3 price">
                               <span>$120</span>
                             </div>
                           </div>
@@ -106,43 +113,44 @@ function MyCart() {
                       </div>
                     </div>
                   </div>
-                  <div class="product">
-                    <div class="row">
-                      <div class="col-md-3">
+                  <div className="product">
+                    <div className="row">
+                      <div className="col-md-3">
                         <img
-                          class="img-fluid mx-auto d-block image"
+                          className="img-fluid mx-auto d-block image"
                           src="image.jpg"
                         />
                       </div>
-                      <div class="col-md-8">
-                        <div class="info">
-                          <div class="row">
-                            <div class="col-md-5 product-name">
-                              <div class="product-name">
+                      <div className="col-md-8">
+                        <div className="info">
+                          <div className="row">
+                            <div className="col-md-5 product-name">
+                              <div className="product-name">
                                 <a href="#">Lorem Ipsum dolor</a>
-                                <div class="product-info">
+                                <div className="product-info">
                                   <div>
-                                    Display: <span class="value">5 inch</span>
+                                    Display:{" "}
+                                    <span className="value">5 inch</span>
                                   </div>
                                   <div>
-                                    RAM: <span class="value">4GB</span>
+                                    RAM: <span className="value">4GB</span>
                                   </div>
                                   <div>
-                                    Memory: <span class="value">32GB</span>
+                                    Memory: <span className="value">32GB</span>
                                   </div>
                                 </div>
                               </div>
                             </div>
-                            <div class="col-md-4 quantity">
-                              <label for="quantity">Quantity:</label>
+                            <div className="col-md-4 quantity">
+                              <label htmlFor="quantity">Quantity:</label>
                               <input
                                 id="quantity"
                                 type="number"
                                 value="1"
-                                class="form-control quantity-input"
+                                className="form-control quantity-input"
                               />
                             </div>
-                            <div class="col-md-3 price">
+                            <div className="col-md-3 price">
                               <span>$120</span>
                             </div>
                           </div>
@@ -152,34 +160,38 @@ function MyCart() {
                   </div>
                 </div>
               </div>
-              <div class="col-md-12 col-lg-4">
-                <div class="summary">
+              <div className="col-md-12 col-lg-4">
+                <div className="summary">
                   <h3>Summary</h3>
-                  <div class="summary-item">
-                    <span class="text">Subtotal</span>
-                    <span class="price">$360</span>
+                  <div className="summary-item">
+                    <span className="text">Subtotal</span>
+                    <span className="price">$360</span>
                   </div>
-                  <div class="summary-item">
-                    <span class="text">Discount</span>
-                    <span class="price">$0</span>
+                  <div className="summary-item">
+                    <span className="text">Discount</span>
+                    <span className="price">$0</span>
                   </div>
-                  <div class="summary-item">
-                    <span class="text">Shipping</span>
-                    <span class="price">$0</span>
+                  <div className="summary-item">
+                    <span className="text">Shipping</span>
+                    <span className="price">$0</span>
                   </div>
-                  <div class="summary-item">
-                    <span class="text">Total</span>
-                    <span class="price">$360</span>
+                  <div className="summary-item">
+                    <span className="text">Total</span>
+                    <span className="price">$360</span>
                   </div>
-                  <PayPalScriptProvider options={{ "client-id": "test" }}>
-                    <PayPalButtons style={{ layout: "horizontal" }} />
-                  </PayPalScriptProvider>
-                  {/* <button
-                    type="button"
-                    class="btn btn-warning btn-lg btn-block w-100"
-                  >
-                    Checkout
-                  </button> */}
+                  <div className="mt-3 text-center ">
+                    <p>Choose payment options:</p>
+                  </div>
+                  {/* Paypal Checkout */}
+                  <div className="container">
+                    <PayPalScriptProvider options={initialOptions}>
+                      <PayPalButtons />
+                    </PayPalScriptProvider>
+                    <p className="text-center">or</p>
+                    <button className="shopping-btn btn btn-warning btn-lg w-100">
+                      <i class="fas fa-truck"></i>&nbsp;Cash on Delivery
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
