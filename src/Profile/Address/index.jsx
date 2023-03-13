67409cc87157f943fb091d8837fdde2e8ca1f3ae
Tomeee <diether.noche@gmail.com>
2023-03-13 15:29:32 +0800

    Add contents for address tab

--- src/Profile/Address/index.jsx
+++ src/Profile/Address/index.jsx
@@ -1,115 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import "./address.css";
 
 function Address() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
       <div className="profile-header container my-3 p-4 border">
         <div className="d-flex justify-content-between">
           <h3>My Addresses</h3>
-          <button className="btn btn-warning text-light">
+          <button className="btn btn-warning text-light" onClick={handleShow}>
             <i class="fas fa-plus"></i>&nbsp;Add New Address
           </button>
         </div>
-
         <hr />
-        {/* Username */}
-        <div className="form-group row mb-3">
-          <label htmlFor="input-username" className="col-sm-2 col-form-label">
-            Username
-          </label>
-          <div className="col-sm-10">
-            <input type="text" className="form-control" id="input-username" />
+        {/* Address body */}
+        <div className="address-body d-flex justify-content-center align-items-center">
+          <div className="row row-cols-1 text-center">
+            <span className="display-3 text-muted">
+              <i class="fas fa-map-marked-alt"></i>
+            </span>
+            <p className="lead">You don't have addresses yet.</p>
           </div>
         </div>
-        {/* Fullname */}
-        <div className="form-group row mb-3">
-          <label htmlFor="input-fullName" className="col-sm-2 col-form-label">
-            Full Name
-          </label>
-          <div className="col-sm-10">
-            <input type="text" className="form-control" id="input-fullName" />
-          </div>
-        </div>
-        {/* Email */}
-        <div className="form-group row mb-3">
-          <label htmlFor="input-email" className="col-sm-2 col-form-label">
-            Email Address
-          </label>
-          <div className="col-sm-10">
-            <input type="email" className="form-control" id="input-email" />
-          </div>
-        </div>
-        {/* Contact No. */}
-        <div className="form-group row mb-3">
-          <label htmlFor="input-number" className="col-sm-2 col-form-label">
-            Contact No.
-          </label>
-          <div className="col-sm-10">
-            <input
-              type="text"
-              className="form-control w-25"
-              id="input-number"
-              maxLength="11"
-            />
-          </div>
-        </div>
-        {/* Gender */}
-        <div className="form-group row mb-3">
-          <label htmlFor="input-gender" className="col-sm-2 col-form-label">
-            Gender
-          </label>
-          <div className="col-sm-10">
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                name="inlineRadioOptions"
-                id="inlineRadio1"
-                value="option1"
-              />
-              <label class="form-check-label" for="inlineRadio1">
-                Male
-              </label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                name="inlineRadioOptions"
-                id="inlineRadio2"
-                value="option2"
-              />
-              <label class="form-check-label" for="inlineRadio2">
-                Female
-              </label>
-            </div>
-            <div class="form-check form-check-inline">
-              <input
-                class="form-check-input"
-                type="radio"
-                name="inlineRadioOptions"
-                id="inlineRadio3"
-                value="option3"
-              />
-              <label class="form-check-label" for="inlineRadio3">
-                Other
-              </label>
-            </div>
-          </div>
-        </div>
-        {/* Date of Birth */}
-        <div className="form-group row mb-3">
-          <label htmlFor="input-birth" className="col-sm-2 col-form-label">
-            Date of Birth
-          </label>
-          <div className="col-sm-10">
-            <input type="date" className="form-control w-25" id="input-birth" />
-          </div>
-        </div>
-        {/* Save Button */}
-        <div className="text-end">
-          <button className="btn btn-warning mt-5 w-25 text-light">Save</button>
-        </div>
+        {/* Modal */}
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>New Address</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Control type="text" placeholder="Full Name" />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="number"
+                  maxLength={11}
+                  placeholder="Contact No."
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="text"
+                  placeholder="Region, Province, City, Barangay"
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Control type="number" placeholder="Postal Code" />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Control
+                  type="text"
+                  placeholder="Street Name, Building, House No."
+                />
+              </Form.Group>
+              <Form.Check label="Set as Default Address" />
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="light" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button variant="warning" onClick={handleClose}>
+              Submit
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     </>
   );
