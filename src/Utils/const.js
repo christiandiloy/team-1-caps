879b6807310362875e6a47e1d3c5eda39767b42f
Tomeee <diether.noche@gmail.com>
2023-03-13 19:25:47 +0800

    Make the change password working

--- src/Utils/const.js
+++ src/Utils/const.js
@@ -1,7 +1,11 @@
 export const myExpressURL = "http://localhost:3005";
 
+const userData = JSON.parse(localStorage.getItem("user"));
+var userId = userData && userData.id ? userData.id : 0;
+
 export const serverRoutes = {
   Login: myExpressURL + "/api/v2/login",
   Register: myExpressURL + "/api/v2/register",
-  Subscriber: myExpressURL + "/subscribe"
+  UpdatePassword: myExpressURL + `/api/v2/users/${userId}/password`,
+  Subscriber: myExpressURL + "/subscribe",
 };
