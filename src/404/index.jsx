20e7cf8711bd954b0a495e4032c1c0770d8af0d3
Tomeee <diether.noche@gmail.com>
2023-03-21 22:13:01 +0800

    Change 404 not fount style

--- src/404/index.jsx
+++ src/404/index.jsx
@@ -1,11 +1,33 @@
-function NotFound() {
-    return (
-        <>
-            <div>
-                <h1>404 PAGE NOT FOUND</h1>
-            </div>
-        </>
-    )
+import "./404.css";
+import { Link } from "react-router-dom";
+
+function NotFound(props) {
+  return (
+    <>
+      <div id="notfound">
+        <div className="notfound">
+          <div className="notfound-404">
+            <h1>
+              4<span>0</span>4
+            </h1>
+          </div>
+          <p className="notfound-paragraph">
+            The page you are looking for might have been removed had its name
+            changed or is temporarily unavailable.
+          </p>
+          <Link
+            to="/"
+            className="anchor"
+            onClick={() => {
+              props.setCurrentLink("/");
+            }}
+          >
+            Home Page
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default NotFound;
